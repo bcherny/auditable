8383254f7523e848d86a3676a06a2cecb39d3a7b
Boris Cherny <bcherny@gmail.com>
2016-07-04 13:45:50 -0700

    clean up tests

--- test.ts
+++ test.ts
@@ -33,21 +33,3 @@ function beforeEach(t: ContextualTestContext): List<number> {
   a[1] = 20
   return a
 }
-
-// const a = new Map({
-//   a: 1,
-//   b: 2
-// })
-
-// const b = a.a = 2
-// const c = b.a = 3
-// const d = c.a = 4
-
-// equal(a.a, 1)
-// equal(b.a, 2)
-// equal(c.a, 3)
-// equal(d.a, 4)
-
-// notEqual(a, b)
-// notEqual(b, c)
-// notEqual(c, d)
