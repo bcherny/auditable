3d178338bb3fbff66eda6d5ce1dcfceb27e31264
Boris Cherny <bcherny@gmail.com>
2016-07-05 18:48:57 -0700

    cleanup, rm dependencies

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
 "use strict";
-const Immutable = require('immutable');
-const lodash_1 = require('lodash');
-const AUDIT = Symbol();
-const AUDIT_WITH_TRACES = Symbol();
+const state = {
+    audits: new WeakMap(),
+    proxyByList: new WeakMap()
+};
 function JustList(...as) {
-    const state = {
-        audit: [
-            { data: Immutable.List(as), trace: null }
-        ]
-    };
     const interceptor = {
         get: function (target, prop) {
+            const audit = state.audits.get(target);
             switch (prop) {
                 case Symbol.toStringTag:
-                    return lodash_1.first(state.audit).data.toJS();
-                case AUDIT:
-                    return state.audit.map(_ => _.data.toJS());
-                case AUDIT_WITH_TRACES:
-                    return state.audit.map(({ data, trace }) => ({
-                        trace: trace,
-                        data: data.toJS()
-                    }));
+                    return audit[0].data;
                 default:
-                    return lodash_1.first(state.audit).data.get(prop);
+                    return audit[0].data[prop];
             }
         },
         set: function (target, prop, value) {
-            switch (prop) {
-                case AUDIT: break;
-                default:
-                    state.audit = [
-                        {
-                            data: lodash_1.first(state.audit).data.set(prop, value),
-                            trace: getTrace()
-                        }
-                    ].concat(state.audit);
-            }
+            const audit = state.audits.get(target);
+            const data = audit[0].data.slice(0); // clone
+            data[prop] = value;
+            const newAudit = {
+                data: data,
+                time: getTime(),
+                trace: getTrace()
+            };
+            state.audits.set(target, [newAudit].concat(audit));
             return true;
         }
     };
-    const p = new Proxy(this, interceptor);
-    p[AUDIT] = () => state.audit;
-    return p;
+    const proxy = new Proxy(this, interceptor);
+    state.audits.set(this, [{
+            data: as,
+            time: getTime(),
+            trace: null
+        }]);
+    state.proxyByList.set(proxy, this);
+    return proxy;
 }
 exports.List = Object.assign(JustList, {
     audit: function audit(a) {
-        return a[AUDIT];
+        const proxy = state.proxyByList.get(a);
+        return state.audits.get(proxy).map(_ => _.data);
     },
     auditWithTraces: function auditWithTraces(a) {
-        return a[AUDIT_WITH_TRACES];
+        const proxy = state.proxyByList.get(a);
+        return state.audits.get(proxy);
     }
 });
+function getTime() {
+    return new Date().toISOString();
+}
 function getTrace() {
     try {
-        throw new Error;
+        throw new Error();
     }
     catch (e) {
         const stack = e.stack.split('\n').slice(3);
--- index.ts
+++ index.ts
@@ -1,88 +1,79 @@
-import * as Immutable from 'immutable'
-import {first} from 'lodash'
-
-const AUDIT = Symbol()
-const AUDIT_WITH_TRACES = Symbol()
-
-export interface List<A> extends Array<A> { }
+export interface List<A> extends Array<A> {}
 
 type Data<A> = A[][]
 
 interface DataWithTraces<A> {
   data: A[]
+  time: string
   trace: string[]
 }
 
 export interface ListConstructor {
-  <A>(...as: A[]): List<A>
+  <A>(...as: A[]): Array<A>
   audit<A>(a: List<A>): Data<A>
   auditWithTraces<A>(a: List<A>): DataWithTraces<A>[]
 }
 
-interface State<A> {
-  audit: {
-    data: Immutable.List<A>,
-    trace: string[]
-  }[]
+const state = {
+  audits: new WeakMap<Array<any>, DataWithTraces<any>[]>(),
+  proxyByList: new WeakMap<Array<any>, Array<any>>()
 }
 
-function JustList <A>(...as: A[]): List<A> {
-
-  const state: State<A> = {
-    audit: [
-      { data: Immutable.List(as), trace: null }
-    ]
-  }
+function JustList<A>(...as: A[]): Array<A> {
 
   const interceptor = {
-    get: function (target: List<A>, prop: number | symbol): A | A[] | Data<A> | {data: A[], trace: string[]}[] {
+    get: function (target: List<A>, prop: number | symbol): A | A[] | Data<A> | { data: A[], trace: string[] }[] {
+      const audit = state.audits.get(target)
       switch (prop) {
         case Symbol.toStringTag:
-          return first(state.audit).data.toJS()
-        case AUDIT:
-          return state.audit.map(_ => _.data.toJS())
-        case AUDIT_WITH_TRACES:
-          return state.audit.map(({data, trace}) => ({
-            trace,
-            data: data.toJS()
-          }))
+          return audit[0].data
         default:
-          return first(state.audit).data.get(prop as number)
+          return audit[0].data[prop as number]
       }
     },
     set: function (target: List<A>, prop: number | symbol, value: A) {
-      switch (prop) {
-        case AUDIT: break
-        default:
-          state.audit = [
-            {
-              data: first(state.audit).data.set(prop as number, value),
-              trace: getTrace()
-            }
-          ].concat(
-            state.audit
-          )
+      const audit = state.audits.get(target)
+      const data = audit[0].data.slice(0) // clone
+      data[prop as number] = value
+      const newAudit: DataWithTraces<A> = {
+        data,
+        time: getTime(),
+        trace: getTrace()
       }
+      state.audits.set(target, [newAudit].concat(audit))
       return true
     }
   }
 
-  const p = new Proxy<List<A>>(this, interceptor)
-  p[AUDIT] = () => state.audit
-  return p
+  const proxy = new Proxy<List<A>>(this, interceptor)
+
+  state.audits.set(this, [{
+    data: as,
+    time: getTime(),
+    trace: null
+  }])
+  state.proxyByList.set(proxy, this)
+
+  return proxy
 }
 
 export const List: ListConstructor = Object.assign(JustList, {
-  audit: function audit <A>(a: List<A>): Data<A> {
-    return a[AUDIT]
+  audit: function audit<A>(a: List<A>): Data<A> {
+    const proxy = state.proxyByList.get(a)
+    return state.audits.get(proxy).map(_ => _.data)
   },
   auditWithTraces: function auditWithTraces<A>(a: List<A>): DataWithTraces<A>[] {
-    return a[AUDIT_WITH_TRACES]
+    const proxy = state.proxyByList.get(a)
+    return state.audits.get(proxy)
   }
 })
 
+function getTime(): string {
+  return new Date().toISOString()
+}
+
 function getTrace(): string[] {
-  try { throw new Error } catch (e) {
+  try { throw new Error() } catch (e) {
     const stack: string[] = e.stack.split('\n').slice(3)
     stack[0] = stack[0].slice(7) // rm leading "     at" on 1st line
     return stack
